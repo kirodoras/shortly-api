28f8bbdafce7f9ae460f9550c9e9080ca21ee7db
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-08-06 16:40:28 -0300

    refactor: query sql string to search user info using left join in - get user info

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -3,35 +3,27 @@ import connection from "../databases/postgres.js";
 export async function getUserInfo(req, res) {
   try {
     const { userId } = res.locals;
-    const { rows: user } = await connection.query(
-      `SELECT users.id, users.name
-        FROM users
-        WHERE users.id = $1`,
-      [userId]
-    );
-    const { rows: info } = await connection.query(
-      `SELECT SUM("visitCount") as "visitCount", 
+    const { rows } = await connection.query(
+      `SELECT users.id, users.name, SUM("visitCount") as "visitCount", 
         json_agg(
-            u.*
+          u.*
         ) as "shortenedUrls"
         FROM users
-        JOIN (
-            SELECT urls.id, urls."userId"
-            FROM urls
+        LEFT JOIN (
+          SELECT urls.id, urls."userId"
+          FROM urls
         ) i
         ON i."userId" = users.id
-        JOIN (
-            SELECT urls.id, urls."shortUrl", urls.url, urls."visitCount"
-            FROM urls
+        LEFT JOIN (
+          SELECT urls.id, urls."shortUrl", urls.url, urls."visitCount"
+          FROM urls
         ) u
         ON i.id = u.id
         WHERE users.id = $1
         GROUP BY users.id`,
       [userId]
     );
-    const response = { ...user[0], ...info[0] };
-    console.log(response);
-    res.status(200).send(response);
+    res.status(200).send(rows[0]);
   } catch (error) {
     res.sendStatus(500);
   }
